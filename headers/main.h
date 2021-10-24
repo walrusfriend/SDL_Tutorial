3816c846d618f8e9b96c85f5a82bb4463ebb91bb
Andrei_Agafonov <vikvas9@yandex.ru>
2021-10-24 16:11:46 +0300

    Add fonts

---
+++ headers/main.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <iso646.h>
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+// Without this we have an error LNK2019: external unresolved symbol main
+#undef main
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include <string>
-#include <iso646.h>
-
-#include <SDL.h>
-#include <SDL_image.h>
-
-// Without this we have an error LNK2019: external unresolved symbol main
-#undef main
+#include "../headers/main.h"
 
 // Set screen size
 const int SCREEN_WIDTH = 640;
@@ -15,9 +7,25 @@ const int SCREEN_HEIGHT = 480;
 // Set tile size
 const int TILE_SIZE = 40;
 
+// Set path to images folder
+std::string imagesPath = "./images/";
+std::string fontsPath = "./fonts/";
+// std::string imagesPath = "../images/";
+
 struct Position {
     int x;
     int y;
+};
+
+enum Directions {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UP_LEFT,
+    UP_RIGHT,
+    DOWN_LEFT,
+    DOWN_RIGHT
 };
 
 SDL_Window* window = nullptr;
@@ -26,23 +34,34 @@ SDL_Renderer* renderer = nullptr;
 // Function defenition
 SDL_Texture* loadImage(std::string&& path);
 void applySurface(int x, int y, SDL_Texture* texture);
-bool renderTexture(SDL_Texture* texture, int x, int y, int w, int h);
-bool renderTexture(SDL_Texture* texture, int x, int y);
+void renderTexture(SDL_Texture* texture, int x, int y, int w, int h);
+void renderTexture(SDL_Texture* texture, SDL_Rect dest, SDL_Rect* clip = nullptr);
+void renderTexture(SDL_Texture* texture, int x, int y, SDL_Rect* clip = nullptr);
 void cleanup();
 
 void renderBackground(SDL_Texture* background);
 bool checkCollision(const Position& pos, int size);
+void charMove(Position& charPos, int charSize, int charStepSize, int direction);
+SDL_Texture* renderText(const std::string& message, TTF_Font* font, SDL_Color color);
+void cerrErrorSDL(std::string&& str) { std::cout << str + " Error: " << SDL_GetError() << std::endl; }
 
 int main(int argc, char** argv) {
     // Initialize SDL
     if (SDL_Init(SDL_INIT_EVERYTHING) == -1) {
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        cerrErrorSDL("SDL_Init");
         return 1;
     }
     
     // Initialize SDL_Image module
     if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
-        std::cerr << "IMG_Init Error: " << IMG_GetError() << std::endl;
+        cerrErrorSDL("IMG_Init");
+        SDL_Quit();
+        return 1;
+    }
+
+    // Initialize TTF module
+    if (TTF_Init() != 0) {
+        cerrErrorSDL("TTF module");
         SDL_Quit();
         return 1;
     }
@@ -51,14 +70,14 @@ int main(int argc, char** argv) {
     window = SDL_CreateWindow("SDL_Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
                               SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
-        std::cerr << "Create Window Error: " << SDL_GetError() << std::endl;
+        cerrErrorSDL("Create Window");
         return 1;
     }
 
     // Create renderer
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == nullptr) {
-        std::cerr << "Create Renderer Error: " << SDL_GetError() << std::endl;
+        cerrErrorSDL("Create Renderer");
         return 1;
     }
 
@@ -67,7 +86,7 @@ int main(int argc, char** argv) {
     SDL_Texture* character = nullptr;
 
     background = loadImage("background.png");
-    character = loadImage("character.png");
+    character = loadImage("ball.png");
     if (!background || !character) {
         SDL_DestroyTexture(background);
         SDL_DestroyTexture(character);
@@ -78,9 +97,45 @@ int main(int argc, char** argv) {
     bool quit = false;
     SDL_Event event;
 
+    int iW = 100;
+    int iH = 100;
+    int x = (SCREEN_WIDTH - iW) / 2;
+    int y = (SCREEN_HEIGHT - iH) / 2;
+
     // Chatacter pos
-    Position charPos { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-    int charSize = 40;
+    Position charPos {x, y};
+    int charStepSize = 10;
+    int charSize = 100;
+
+    SDL_Rect clips[4];
+    for (int i = 0; i < 4; ++i) {
+        clips[i].x = i / 2 * iW;
+        clips[i].y = i % 2 * iH;
+        clips[i].w = iW;
+        clips[i].h = iH;
+    }
+
+    int useClip = 0;
+
+    // Setting the font
+    SDL_Color fontColor = {255, 255, 255, 255};
+    TTF_Font* font = TTF_OpenFont((fontsPath + "sample.ttf").c_str(), 64);
+    if (font == nullptr) {
+        cerrErrorSDL("Open font");
+        return 1;
+    }
+
+    SDL_Texture* textImage = renderText("TTF fonts are cool!", font, fontColor);
+    if (textImage == nullptr) {
+        cleanup();
+    }
+
+    int fontWidth;
+    int fontHeight;
+    SDL_QueryTexture(textImage, nullptr, nullptr, &fontWidth, &fontHeight);
+    int fontX = (SCREEN_WIDTH - fontWidth) / 2;
+    int fontY = 0;
+
 
     // main loop
     while (!quit) {
@@ -92,35 +147,30 @@ int main(int argc, char** argv) {
             if (event.type == SDL_KEYDOWN) {
                 const uint8_t* state = SDL_GetKeyboardState(NULL);
                 // Diagional movement
-                if (state[SDL_SCANCODE_W] and state[SDL_SCANCODE_A]) {
-                    Position nextPos {charPos.x, charPos.y - charSize};
-                    if (!checkCollision(nextPos, charSize))
-                        charPos.y -= charSize;
-
-                    nextPos.x = charPos.x - charSize;
-                    if (!checkCollision(nextPos, charSize))
-                        charPos.x -= charSize;
-                }
-                else if (state[SDL_SCANCODE_W]) {
-                    Position nextPos {charPos.x, charPos.y - charSize};
-                    if (!checkCollision(nextPos, charSize))
-                        charPos.y -= charSize;
-                }
-                else if (state[SDL_SCANCODE_S]) {
-                    Position nextPos {charPos.x, charPos.y + charSize};
-                    if (!checkCollision(nextPos, charSize))
-                        charPos.y += charSize;
-                }
-                else if (state[SDL_SCANCODE_A]) {
-                    Position nextPos {charPos.x - charSize, charPos.y};
-                    if (!checkCollision(nextPos, charSize))
-                        charPos.x -= charSize;
-                }
-                else if (state[SDL_SCANCODE_D]) {
-                    Position nextPos {charPos.x + charSize, charPos.y};
-                    if (!checkCollision(nextPos, charSize))
-                        charPos.x += charSize;
-                }
+                if (state[SDL_SCANCODE_1])
+                    useClip = 0;
+                else if (state[SDL_SCANCODE_2])
+                    useClip = 1;
+                else if (state[SDL_SCANCODE_3])
+                    useClip = 2;
+                else if (state[SDL_SCANCODE_4])
+                    useClip = 3;
+                else if (state[SDL_SCANCODE_W] and state[SDL_SCANCODE_A])
+                    charMove(charPos, charSize, charStepSize, UP_LEFT);
+                else if (state[SDL_SCANCODE_W] and state[SDL_SCANCODE_D])
+                    charMove(charPos, charSize, charStepSize, UP_RIGHT);
+                else if (state[SDL_SCANCODE_S] and state[SDL_SCANCODE_A])
+                    charMove(charPos, charSize, charStepSize, DOWN_LEFT);
+                else if (state[SDL_SCANCODE_S] and state[SDL_SCANCODE_D])
+                    charMove(charPos, charSize, charStepSize, DOWN_RIGHT);
+                else if (state[SDL_SCANCODE_W])
+                    charMove(charPos, charSize, charStepSize, UP);
+                else if (state[SDL_SCANCODE_S])
+                    charMove(charPos, charSize, charStepSize, DOWN);
+                else if (state[SDL_SCANCODE_A])
+                    charMove(charPos, charSize, charStepSize, LEFT);
+                else if (state[SDL_SCANCODE_D])
+                    charMove(charPos, charSize, charStepSize, RIGHT);
                 else if (state[SDL_SCANCODE_ESCAPE])
                     quit = true;
             }
@@ -129,7 +179,8 @@ int main(int argc, char** argv) {
         // Clear window
         SDL_RenderClear(renderer);
         renderBackground(background);
-        renderTexture(character, charPos.x, charPos.y, charSize, charSize);
+        renderTexture(textImage, fontX, fontY);
+        renderTexture(character, charPos.x, charPos.y, &clips[useClip]);
         SDL_RenderPresent(renderer);
 
         // Free CPU resources
@@ -143,59 +194,71 @@ int main(int argc, char** argv) {
 
 /**
  * Load texture from file on the path "path"
- * @param  path    Path to the file
- * @return Returns texture or nullptr if error occurred
+ * @param  path     Path to the file
+ * @return          Returns texture or nullptr if error occurred
 */
 SDL_Texture* loadImage(std::string&& path) {
     // Load image
-    SDL_Texture* texture = IMG_LoadTexture(renderer, path.c_str());
+    SDL_Texture* texture = IMG_LoadTexture(renderer, (imagesPath + path).c_str());
     if (!texture) {
-        std::cerr << "Load Image Error: " << IMG_GetError() << std::endl;
+        cerrErrorSDL("Load Image");
     }
     return texture;
 }
 
 /**
  * Draw image on renderer with scaling
- * @param texture Texture for drawing
- * @param x X coordinate
- * @param y Y coordinate
- * @param w Scaling width
- * @param h Scaling height
- * @return Returns true if success or false
+ * @param texture   Pointer to the texture to draw
+ * @param x         X coordinate of the drawing location
+ * @param y         Y coordinate of the drawing location
+ * @param w         Scaling width
+ * @param h         Scaling height
+ * @return          None
  */
-bool renderTexture(SDL_Texture* texture, int x, int y, int w, int h) {
+void renderTexture(SDL_Texture* texture, int x, int y, int w, int h) {
     SDL_Rect rect;
     rect.x = x;
     rect.y = y;
     rect.w = w;
     rect.h = h;
     if (SDL_RenderCopy(renderer, texture, NULL, &rect) != 0) {
-        std::cerr << "Render Texture Error: " << SDL_GetError() << std::endl;
-        return false;
+        cerrErrorSDL("Render Texture");
     }
-    return true;
 }
 
 /**
- * Draw image on renderer without scaling
- * @param texture Texture for drawing
- * @param x X coordinate
- * @param y Y coordinate
- * @return Returns true if success or false
+ * Draw image to target rect
+ * @param texture   Pointer to the texture to draw
+ * @param target    The rect in which you will draw
+ * @param clip      A part of the image that will draw (nullptr will draw whole image)
+ * @return          None
  */
-bool renderTexture(SDL_Texture* texture, int x, int y) { 
-    int w, h;
-    // Get image size
-    if (SDL_QueryTexture(texture, NULL, NULL, &w, &h) != 0) {
-        std::cerr << SDL_GetError() << std::endl;
-        return false;
-    }
+void renderTexture(SDL_Texture* texture, SDL_Rect target, SDL_Rect* clip) {
+    SDL_RenderCopy(renderer, texture, clip, &target);
+}
 
-    if (renderTexture(texture, x, y, w, h) == false) {
-        return false;
+/**
+ * Draw original image or its part to certain point (x,y)
+ * If you draw the whole image, its size will be used
+ * If you draw only a part of the image, the size of the part will be used
+ * @param texture   Pointer to the texture to draw
+ * @param x         X coordinate of the drawing location
+ * @param y         Y coordinate of the drawing location
+ * @param clip      A part of the image that will draw
+ * @return          None
+ */
+void renderTexture(SDL_Texture* texture, int x, int y, SDL_Rect* clip) {
+    SDL_Rect target;
+    target.x = x;
+    target.y = y;
+    if (clip != nullptr) {
+        target.w = clip->w;
+        target.h = clip->h;
     }
-    return true;
+    else {
+        SDL_QueryTexture(texture, nullptr, nullptr, &target.w, &target.h);
+    }
+    renderTexture(texture, target, clip);
 }
 
 /**
@@ -205,14 +268,15 @@ bool renderTexture(SDL_Texture* texture, int x, int y) {
 void cleanup() {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_Quit();
     IMG_Quit();
     SDL_Quit();
 }
 
 /**
  * Draw background
- * @param background Pointer to image
- * @return None
+ * @param background    Pointer to image
+ * @return              None
  */
 void renderBackground(SDL_Texture* background) {
     int xTiles = SCREEN_WIDTH / TILE_SIZE;
@@ -227,9 +291,9 @@ void renderBackground(SDL_Texture* background) {
 
 /**
  * Check if we hit to another object
- * @param pos Coordinates of the object
- * @param size Size of texteru
- * @return true if hit else false
+ * @param pos   Coordinates of the object
+ * @param size  Size of texture
+ * @return      True if hit else false
  */
 bool checkCollision(const Position& pos, int size) {
     // Check X direction
@@ -241,4 +305,81 @@ bool checkCollision(const Position& pos, int size) {
         return true;
 
     return false;
+}
+
+void charMove(Position& charPos, int charSize, int charStepSize, int direction) {
+    Position nextPos = {charPos.x, charPos.y};
+    switch (direction) {
+        case UP:
+            nextPos.y -= charStepSize;
+            if (!checkCollision(nextPos, charSize))
+                charPos = nextPos;
+            break;
+        case DOWN:
+            nextPos.y += charStepSize;
+            if (!checkCollision(nextPos, charSize))
+                charPos = nextPos;
+            break;
+        case LEFT:
+            nextPos.x -= charStepSize;
+            if (!checkCollision(nextPos, charSize))
+                charPos = nextPos;
+            break;
+        case RIGHT:
+            nextPos.x += charStepSize;
+            if (!checkCollision(nextPos, charSize))
+                charPos = nextPos;
+            break;
+        case UP_LEFT:
+            charMove(charPos, charSize, charStepSize, UP);
+            charMove(charPos, charSize, charStepSize, LEFT);
+            break;
+        case UP_RIGHT:
+            charMove(charPos, charSize, charStepSize, UP);
+            charMove(charPos, charSize, charStepSize, RIGHT);
+            break;
+        case DOWN_LEFT:
+            charMove(charPos, charSize, charStepSize, DOWN);
+            charMove(charPos, charSize, charStepSize, LEFT);
+            break;
+        case DOWN_RIGHT:
+            charMove(charPos, charSize, charStepSize, DOWN);
+            charMove(charPos, charSize, charStepSize, RIGHT);
+            break;
+        default:
+            std::cerr << "ERROR: Character movement error!" << std::endl;
+            break;
+    }
+}
+
+/**
+ * Draw a text to the screen
+ * @param message   Text to be written
+ * @param font      Pointer to TTF_Font object
+ * @param color     Color of the text
+ * @param fontSize  Size of the text
+ * @return          Pointer to the texture or nullptr if something went wrong
+ */
+SDL_Texture* renderText(const std::string& message, TTF_Font* font, SDL_Color color) {
+    if (font == nullptr) {
+        std::cerr << "RenderText Error: Empty font file!" << std::endl;
+        return nullptr;
+    }
+
+    // Project text to the surface
+    SDL_Surface* surface = TTF_RenderText_Blended(font, message.c_str(), color);
+    if (surface == nullptr) {
+        cerrErrorSDL("TTF_RenderText");
+        return nullptr;
+    }
+
+    // Transform surface to texture
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (texture == nullptr) {
+        cerrErrorSDL("Texture creation");
+    }
+
+    // Free memory
+    SDL_FreeSurface(surface);
+    return texture;
 }
